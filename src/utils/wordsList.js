7b5d3fdb91d7ce39fa0e54cf723158e16ca5ee20
Vyacheslav <vskorop@gmail.com>
2022-08-27 16:23:02 +0300

    add funct reverse for arrays

--- src/utils/wordsList.js
+++ src/utils/wordsList.js
@@ -1,13 +1,33 @@
-const wordsList = [
-  'МОРЖ',
-  'РУГО',
-  'ЕЦАЗ',
-  'АКУБ'
+// let wordsList = [
+//   'МОРЖ',
+//   'РУГО',
+//   'ЕЦАЗ',
+//   'АКУБ'
+// ];
+const list = (arr, n) => {
+  const res = [];
+  return arr.join('')
+    .match(/.{1,4}/g)
+    .map((el, index) => {
+      if (index % 2 === 0) return el.split('').reverse().join('');
+      return el;
+    });
+};
+const list2 = (arr, n) => {
+  const res = [];
+  return arr.join('')
+    .match(/.{1,3}/g)
+    .map((el, index) => {
+      if (index % 2 === 0) return el.split('').reverse().join('');
+      return el;
+    });
+};
+const wordsList = list(['МОРЖ', 'ОГУРЕЦ', 'АЗБУКА'], 4);
+const wordsList2 = list2(['МЕНЮ', 'УКСУС'], 3);
 
-];
-const wordsList2 = [
-  'МЕН',
-  'КУЮ',
-  'СУС',
-];
+// const wordsList2 = [
+//   'МЕН',
+//   'КУЮ',
+//   'СУС',
+// ];
 export { wordsList, wordsList2 };
